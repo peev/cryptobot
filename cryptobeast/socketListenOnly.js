17506d99c6733da54949a1a7dc02085952529a0c
peev <christo.peev@bisam.com>
2017-07-07 20:38:46 +0300

    asd

--- cryptobeast/socketListenOnly.js
+++ cryptobeast/socketListenOnly.js
@@ -13,7 +13,7 @@ console.log = function (d) { //
 };
 
 // mongoose.connect('mongodb://localhost/BittrexMarketsState3');
-mongoose.connect('mongodb://cb-admin:0887pass@ec2-34-203-208-180.compute-1.amazonaws.com:27017/cryptobotDB2');
+mongoose.connect('mongodb://cb-admin:0887pass@ec2-34-203-208-180.compute-1.amazonaws.com:27017/cryptobotDB');
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose default connection open.');
